7c08766fc27146f0298acf73a99ab8e85408f6da
Yuta <y.sugiyama54@gmail.com>
2020-07-13 22:11:12 +0900

    静的サイト生成の設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,9 @@
 const config = require('./.contentful.json')
 const contentful = require('contentful')
+const client = contentful.createClient({
+  space: config.CTF_SPACE_ID,
+  accessToken: config.CTF_CDA_ACCESS_TOKEN
+})
 export default {
   /*
   ** Nuxt rendering mode
@@ -70,6 +74,27 @@ export default {
   },
   fontawesome: {
     component: 'fa'
+  },
+  generate: {
+    routes () {
+      return Promise.all([
+        client.getEntries({
+          'content_type': 'work'
+        }),
+        client.getEntries({
+          'content_type': 'category'
+        }),
+        client.getEntries({
+          'content_type': 'tag'
+        })
+      ]).then(([works,categories,tags]) => {
+        return [
+          ...works.items.map(work => `work/${work.fields.slug}`),
+          ...categories.items.map(category => `category/${category.fields.slug}`),
+          ...tags.items.map(tag => `tag/${tag.sys.id}`)
+        ]
+      })
+    }
   },
   /*
   ** Build configuration
